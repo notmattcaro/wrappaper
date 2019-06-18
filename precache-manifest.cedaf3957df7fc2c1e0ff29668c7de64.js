7e1adbc99b83741c3491132cc0fb5f3df7339863
notmsc <mscaro22@gmail.com>
2019-06-18 10:19:38 -0700

    Updates

---
+++ precache-manifest.cedaf3957df7fc2c1e0ff29668c7de64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765b0807a0f1ca3ad401b4ead7bb4a8b",
+    "url": "/wrappaper/index.html"
+  },
+  {
+    "revision": "6e43b97c6c17367138ba",
+    "url": "/wrappaper/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/wrappaper/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6e43b97c6c17367138ba",
+    "url": "/wrappaper/static/js/main.1cc2c368.chunk.js"
+  },
+  {
+    "revision": "e583cbdfaab567439670",
+    "url": "/wrappaper/static/js/runtime~main.c0098ced.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wrappaper/static/media/logo.5d5d9eef.svg"
+  }
+]);
